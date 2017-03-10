835205ab36792375adf43f7a2d8955816c0d4df7
teo <ramones.teo@gmail.com>
2017-03-10 13:23:56 +0200

    Adds Contract file for the database

---
+++ app/src/main/java/pop/moviesdb/popularmoviesudacity/data/MoviesDbHelper.java
@@ -0,0 +1,43 @@
+package pop.moviesdb.popularmoviesudacity.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static android.provider.BaseColumns._ID;
+import static pop.moviesdb.popularmoviesudacity.data.MoviesContract.*;
+
+/**
+ * Helper class for our database.
+ */
+
+public class MoviesDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "movies.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MoviesDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        final String SQL_CREATE_FAVORITES_TABLE = "CREATE TABLE " +
+                Favorites.TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                Favorites.COLUMN_MOVIE_ID + " INTEGER NOT NULL" +
+                Favorites.COLUMN_TITLE + " TEXT NOT NULL" +
+                Favorites.COLUMN_POSTER_PATH + " TEXT NOT NULL" +
+                ");";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_FAVORITES_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Favorites.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
