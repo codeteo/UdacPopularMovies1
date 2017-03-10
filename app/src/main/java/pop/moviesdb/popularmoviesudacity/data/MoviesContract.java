775cbd1770bf64a76c8f60d4f3021d3bd7792f5e
teo <ramones.teo@gmail.com>
2017-03-10 12:54:19 +0200

    Adds Contract class for database

---
+++ app/src/main/java/pop/moviesdb/popularmoviesudacity/data/MoviesContract.java
@@ -0,0 +1,20 @@
+package pop.moviesdb.popularmoviesudacity.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Contract class for Database.
+ */
+
+public class MoviesContract {
+
+    public static final class Favorites implements BaseColumns {
+
+        public static final String TABLE_NAME = "favorites";
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_MTITLE = "title";
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+
+    }
+
+}
