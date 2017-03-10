e204b6e17b0b1f7a1a41ee32d561a63674402394
teo <ramones.teo@gmail.com>
2017-03-10 16:03:12 +0200

    Adds UriMatcher to ContentProvider

--- app/src/main/java/pop/moviesdb/popularmoviesudacity/data/MoviesContentProvider.java
+++ app/src/main/java/pop/moviesdb/popularmoviesudacity/data/MoviesContentProvider.java
@@ -3,17 +3,37 @@ package pop.moviesdb.popularmoviesudacity.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+
+import static pop.moviesdb.popularmoviesudacity.data.MoviesContract.AUTHORITY;
+import static pop.moviesdb.popularmoviesudacity.data.MoviesContract.PATH_FAVORITE;
 
 /**
  * ContentProvider class for our app to provide access to database's operations.
  */
 
-public class MoviesContentProvides extends ContentProvider {
+public class MoviesContentProvider extends ContentProvider {
+
+    public static final int FAVORITES = 100;
+    public static final int FAVORITES_WITH_ID = 101;
 
     private MoviesDbHelper moviesDbHelper;
+
+    public static final UriMatcher uriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        // "favorites" directory
+        uriMatcher.addURI(AUTHORITY, PATH_FAVORITE, FAVORITES);
+        // single item from "favorites"
+        uriMatcher.addURI(AUTHORITY, PATH_FAVORITE + "/#", FAVORITES_WITH_ID);
+
+        return uriMatcher;
+    }
 
     @Override
     public boolean onCreate() {
